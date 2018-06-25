14def7b3d33be6b5d20a0f89761f62f20b6c5764
Kayrat Rakhimov <Kayrat_Rakhimov@epam.com>
2018-06-25 14:27:53 +0600

    Added JSON format report for Cucumber

--- src/test/java/com/kairatr/bdd/integration/IntegrationTest.java
+++ src/test/java/com/kairatr/bdd/integration/IntegrationTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber"})
+@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class IntegrationTest {
 
 }
