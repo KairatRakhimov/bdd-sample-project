4c26999db757ceddd5c22a05f5d279d5653483a1
Kayrat Rakhimov <Kayrat_Rakhimov@epam.com>
2018-06-26 14:39:42 +0600

    Removed unrelated unit tests

--- src/test/java/com/kairatr/bdd/integration/IntegrationTest.java
+++ src/test/java/com/kairatr/bdd/integration/IntegrationTest.java
@@ -3,7 +3,9 @@ package com.kairatr.bdd.integration;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class IntegrationTest {
